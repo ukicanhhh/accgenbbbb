2c43104a29c8df60e926fca9164b21d7d3fe0111
ukicanhhh <101096117+ukicanhhh@users.noreply.github.com>
2022-03-07 17:50:32 +0300

    Update new.py

--- new.py
+++ new.py
@@ -1,9 +1,9 @@
-key='e36bb985-e2f1-43fd-b85e-e4569be5d05b'
-app_name="accgen123"
+key='64de64a5-c07a-4e99-aef3-efa52bc2842f'
+app_name="accgen129"
 deviceid="17A7F633BD2668F5C58AC8FF2E0DDB52FE738274220CF20A44FBD5C421B24F47D483B642ED67AE381E"
-emaill="e8bf0gjt9@xojxe.com"
-passwordd="tempmail"
+emaill="wacher12@yandex.ru"
+passwordd="shreck666@1"
 custompwd="fakeaccpwd"
 url="https://ElatedSlipperyArchive.crimsondead.repl.co"
-chatlink="http://aminoapps.com/p/x8nptuj"
-nickname="itsme"
+chatlink="http://aminoapps.com/p/in5z0p"
+nickname="wacher12"
